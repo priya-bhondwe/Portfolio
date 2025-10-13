f3083876b9c806a667501e40a079a70554e35b3e
priya-bhondwe <149312604+priya-bhondwe@users.noreply.github.com>
2025-10-13 21:40:16 +0530

    Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,6 +21,14 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={figma}
+              title="Responsive E-Commerce Website UI/UX Design (Figma Prototype)"
+              description="Designed a modern, responsive e-commerce interface using Figma, focusing on intuitive navigation, clean layout, and engaging product presentation.The prototype includes key user flows such as homepage browsing, product details, cart, and checkout. Emphasized usability, consistency in design elements, and mobile-friendly layouts to enhance user experience."
+              demoLink="https://www.figma.com/proto/wdOoSL72wVKe8LfWPzpMRd/Untitled?node-id=177-123&t=qPIGgDJs5UgAWUeS-1&starting-point-node-id=165%3A10"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={hotel}
               isBlog={false}
               title="Hotel Order"
@@ -43,14 +51,6 @@ function Projects() {
               // imgPath={tourist}
               title="Tourist Website"
               description="Tourist Guide is a web (or mobile-responsive) frontend application aimed at helping travelers explore destinations, plan itineraries, and discover points of interest. It acts as the user-facing interface for a larger tourism platform."
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={figma}
-              title="Responsive E-Commerce Website UI/UX Design (Figma Prototype)"
-              description="Designed a modern, responsive e-commerce interface using Figma, focusing on intuitive navigation, clean layout, and engaging product presentation.The prototype includes key user flows such as homepage browsing, product details, cart, and checkout. Emphasized usability, consistency in design elements, and mobile-friendly layouts to enhance user experience."
-              demoLink="https://www.figma.com/proto/wdOoSL72wVKe8LfWPzpMRd/Untitled?node-id=177-123&t=qPIGgDJs5UgAWUeS-1&starting-point-node-id=165%3A10"
             />
           </Col>
         </Row>
