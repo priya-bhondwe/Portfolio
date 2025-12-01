bc3f3fa8c9845c6b1f6bc716c73c33a180706df9
Bhondwe Priya Rama <priyabhondwe12@gmail.com>
2025-12-01 16:57:01 +0530

    update link

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,6 +21,15 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
+              isBlog={false}
+              imgPath={tourist}
+              title="Tourist Website"
+              description="Tourist Guide is a web (or mobile-responsive) frontend application aimed at helping travelers explore destinations, plan itineraries, and discover points of interest. It acts as the user-facing interface for a larger tourism platform."
+              demoLink="https://triptastic-pune.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={figma}
               title="Responsive E-Commerce Website UI/UX Design (Figma Prototype)"
               description="Designed a modern, responsive e-commerce interface using Figma, focusing on intuitive navigation, clean layout, and engaging product presentation.The prototype includes key user flows such as homepage browsing, product details, cart, and checkout. Emphasized usability, consistency in design elements, and mobile-friendly layouts to enhance user experience."
@@ -40,17 +49,9 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              // imgPath={Luxe}
+              imgPath={Luxe}
               title="Luxe Gleam"
               description="Luxe Gleam is a web application for managing and showcasing jewelry collections, built with modern web technologies. It provides both a customer-facing storefront and an admin panel for managing product listings, categories, inventories, and orders."
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              // imgPath={tourist}
-              title="Tourist Website"
-              description="Tourist Guide is a web (or mobile-responsive) frontend application aimed at helping travelers explore destinations, plan itineraries, and discover points of interest. It acts as the user-facing interface for a larger tourism platform."
             />
           </Col>
         </Row>
